9d764514f7f0dcebca46e1f91411affe2e135120
Le Minh Nhat Dang <lmndang.toronto@gmail.com>
2022-07-04 16:24:10 -0400

    Update create menu

--- components/CreateScreen.js
+++ components/CreateScreen.js
@@ -1,13 +1,51 @@
 import * as React from "react";
 import { Text, View } from "react-native";
+import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
 import styles from "./Styles";
+
+const CakeMenu = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Cake</Text>
+    </View>
+  );
+};
+
+const WaffleMenu = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Waffle</Text>
+    </View>
+  );
+};
+
+const IceCreamMenu = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Ice Cream</Text>
+    </View>
+  );
+};
+
+const Cupcake = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Cupcake</Text>
+    </View>
+  );
+};
+
+const Tab = createMaterialTopTabNavigator();
 
 //Create
 const CreateScreen = () => {
   return (
-    <View style={styles.container}>
-      <Text>Create your dessert!</Text>
-    </View>
+    <Tab.Navigator>
+      <Tab.Screen name="Cake" component={CakeMenu} />
+      <Tab.Screen name="Waffle" component={WaffleMenu} />
+      <Tab.Screen name="Ice Cream" component={IceCreamMenu} />
+      <Tab.Screen name="Cupcake" component={Cupcake} />
+    </Tab.Navigator>
   );
 };
 export default CreateScreen;
