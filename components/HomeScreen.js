eed4cf7f92f9614aeb73c9c14842913dbcde609c
Le Minh Nhat Dang <lmndang.toronto@gmail.com>
2022-07-04 20:44:01 -0400

    Edit home screen

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -1,13 +1,38 @@
 import * as React from "react";
-import { Text, View } from "react-native";
-import styles from "./Styles";
+import { Text, View, StyleSheet } from "react-native";
+//import styles from "./Styles";
 
 //Home Screen
 const HomeScreen = () => {
   return (
     <View style={styles.container}>
-      <Text>This is home!</Text>
+      <View style={styles.box}>
+        <Text>Best Seller</Text>
+      </View>
+
+      <View>
+        <Text>Top Creation</Text>
+      </View>
     </View>
   );
 };
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "left",
+  },
+
+  box:{
+    borderWidth: 1,
+    borderColor: "red",
+    textAlign: "left",
+    height: 100,
+    width: 100,
+    marginBottom: 100,
+    marginLeft: 100
+  }
+});
 export default HomeScreen;
