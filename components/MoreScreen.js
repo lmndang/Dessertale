a198eaec665b79a1e520d98478946d88f40d15a3
Le Minh Nhat Dang <lmndang.toronto@gmail.com>
2022-07-07 16:10:27 -0400

    Fix more view

--- components/MoreScreen.js
+++ components/MoreScreen.js
@@ -1,80 +1,82 @@
 import * as React from "react";
-import { Text, View, StyleSheet, Image } from "react-native";
+import { Text, View, StyleSheet, Image, ScrollView } from "react-native";
 import MapView from "react-native-maps";
 //import styles from "./Styles";
 
 //More Screen
 const MoreScreen = () => {
   return (
-    <View style={styles.container}>
-      <View style={styles.imgContainer}>
-        <Image style={styles.image} source={require("../assets/logo.png")} />
-      </View>
-      <View>
-        <Text style={styles.title}>About us!</Text>
-        <Text style={styles.text}>
-          Dessertale is an app and a service focused on taste, availability &
-          delight for everyone. We believe, that you should not be worrying
-          about the dietary restrictions and still be able to enjoy the sweet
-          part of the world!
-        </Text>
-        <Text style={styles.text}>
-          In our app, everything is easily accessible and customizable. People
-          with health restrictions, allergies, and special food requirements
-          have the exact nutritional information about the selected dessert
-          within a couple of seconds.
-        </Text>
+    <ScrollView>
+      <View style={styles.container}>
+        <View style={styles.imgContainer}>
+          <Image style={styles.image} source={require("../assets/logo.png")} />
+        </View>
+        <View>
+          <Text style={styles.title}>About us!</Text>
+          <Text style={styles.text}>
+            Dessertale is an app and a service focused on taste, availability &
+            delight for everyone. We believe, that you should not be worrying
+            about the dietary restrictions and still be able to enjoy the sweet
+            part of the world!
+          </Text>
+          <Text style={styles.text}>
+            In our app, everything is easily accessible and customizable. People
+            with health restrictions, allergies, and special food requirements
+            have the exact nutritional information about the selected dessert
+            within a couple of seconds.
+          </Text>
 
-        {/* <Text style={styles.text}>
+          {/* <Text style={styles.text}>
           Customers can avoid long waiting queues at the bakery and all the
           uncomfortable business around it. Payment, shipping, and the overall
           shopping experience becomes a problem of the past. On top of it, our
           customers have a positive feeling and a better mood after having done
           the whole process in a couple of clicks!
         </Text> */}
-        <Text style={styles.title}>Our Locations</Text>
-        <View>
-          <MapView
-            style={styles.map}
-            // showsUserLocation
-            // followUserLocation
-            // initialRegion={{
-            //   latitude: 43.761539,
-            //   longitude: -79.411079,
-            //   latitudeDelta: 0.25,
-            //   longitudeDelta: 0.04,
-            // }}
-          >
-            <MapView.Marker
-              title="Dessertale at Bayview Ave"
-              description="Dessert shop"
-              coordinate={{
-                latitude: 43.80923097725955,
-                longitude: -79.3994080192205,
-              }}
-            />
+          <Text style={styles.title}>Our Locations</Text>
+          <View>
+            <MapView
+              style={styles.map}
+              // showsUserLocation
+              // followUserLocation
+              // initialRegion={{
+              //   latitude: 43.761539,
+              //   longitude: -79.411079,
+              //   latitudeDelta: 0.25,
+              //   longitudeDelta: 0.04,
+              // }}
+            >
+              <MapView.Marker
+                title="Dessertale at Bayview Ave"
+                description="Dessert shop"
+                coordinate={{
+                  latitude: 43.80923097725955,
+                  longitude: -79.3994080192205,
+                }}
+              />
 
-            <MapView.Marker
-              title="Dessertale at Finch Ave"
-              description="Dessert shop"
-              coordinate={{
-                latitude: 43.75782426886745,
-                longitude: -79.52879503856856,
-              }}
-            />
+              <MapView.Marker
+                title="Dessertale at Finch Ave"
+                description="Dessert shop"
+                coordinate={{
+                  latitude: 43.75782426886745,
+                  longitude: -79.52879503856856,
+                }}
+              />
 
-            <MapView.Marker
-              title="Dessertale at Downtown Toronto"
-              description="Dessert shop"
-              coordinate={{
-                latitude: 43.65427227229885,
-                longitude: -79.39222970122505,
-              }}
-            />
-          </MapView>
+              <MapView.Marker
+                title="Dessertale at Downtown Toronto"
+                description="Dessert shop"
+                coordinate={{
+                  latitude: 43.65427227229885,
+                  longitude: -79.39222970122505,
+                }}
+              />
+            </MapView>
+          </View>
         </View>
       </View>
-    </View>
+    </ScrollView>
   );
 };
 
